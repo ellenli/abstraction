51ecb2bf339a53b4d6244feb4f302958bb8c1fbf
Ellen Li <ellen.li@shopify.com>
2016-11-29 00:16:33 -0500

    web-ready and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Abstraction
+
+A comic by Shintaro Kago. Read it at http://ellen.li/abstraction.
---
+++ index.php
@@ -0,0 +1,13 @@
+<html><head>
+<title>Abstraction by Shintaro Kago</title>
+<style type="text/css">
+body {
+background-color: #fff;
+margin: -10px;
+overflow-x: hidden;
+}
+</style>
+</head><body>
+<img src="index.jpg" width="75%">
+</body>
+</html>
